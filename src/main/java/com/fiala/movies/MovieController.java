d76ffe46a531292a49e6a0652e78d813c548ee47
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-01-27 18:10:51 +0200

    Create MovieController

---
+++ src/main/java/com/fiala/movies/MovieController.java
@@ -0,0 +1,34 @@
+package com.fiala.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService moviesService;
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(moviesService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/byId/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovieById(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<Movie>>(moviesService.singleMovie(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/byImdbId/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovieByImdbId(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(moviesService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
