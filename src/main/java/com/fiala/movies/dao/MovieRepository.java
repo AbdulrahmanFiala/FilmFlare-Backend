747034116c9216dfb3819148b3926023d934dd45
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-04-15 07:10:47 +0200

    Changing the name of the method
    Changing the name of the method to adhere to Spring Data naming convention

--- src/main/java/com/fiala/movies/dao/MovieRepository.java
+++ src/main/java/com/fiala/movies/dao/MovieRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-    Optional<Movie> findMovieByImdbId(String imdbId);
+    Optional<Movie> findByImdbId(String imdbId);
 
 }
